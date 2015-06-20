a1402f04d1c98586cb90f4322e496977e025bd81
Austin Yarger <ayarger@umich.edu>
2015-06-20 12:57:26 -0700

    Big things

--- Assets/Game_Logic_Controller.cs
+++ Assets/Game_Logic_Controller.cs
@@ -8,13 +8,27 @@ public class Game_Logic_Controller : MonoBehaviour {
 	public int counter = 0;
 	public int MAXCOUNTER = 30;
 	public int incrementer = 1;
+
+	public GameObject plane1;
+	public GameObject plane2;
+	public GameObject plane3;
+	public GameObject plane4;
+
+	public GameObject score_label;
+
+	public int score = 0;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		score ++;
+		score_label.GetComponent<TextMesh> ().text = score.ToString ();
+
+
 		foreach (GameObject cube in list){
 			cube.transform.Translate (new Vector3 (velocity, 0, 0));
 		}
@@ -22,12 +36,21 @@ public class Game_Logic_Controller : MonoBehaviour {
 		if (counter == MAXCOUNTER) {
 			//velocity*=1.01f;
 			//for(int i = 0; i<incrementer; i++){
-			GameObject new_cube = Instantiate (cube_prefab, new Vector3 (-50, 0, UnityEngine.Random.Range (-20, 20) + transform.position.z), Quaternion.identity) as GameObject;
+			GameObject new_cube = Instantiate (cube_prefab, new Vector3 (-50, 0, UnityEngine.Random.Range (-40, 40) + transform.position.z), Quaternion.identity) as GameObject;
 			new_cube.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value, 1);
 			list.Add (new_cube);
 			//}
 			counter=0;
 			incrementer++;
+		}
+
+		plane1.transform.Translate (velocity, 0, 0);
+		plane2.transform.Translate (velocity, 0, 0);
+		plane3.transform.Translate (velocity, 0, 0);
+		plane4.transform.Translate (velocity, 0, 0);
+
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("test");
 		}
 	}
 }
